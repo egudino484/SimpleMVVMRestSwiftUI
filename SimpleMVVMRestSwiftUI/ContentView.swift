8800d8e194ba2aeaf35114860b42db972d10f948
Edison Gudiño <egudino@montran.com>
2020-12-10 15:24:57 -0500

    Initial Commit

---
+++ SimpleMVVMRestSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleMVVMRestSwiftUI
+//
+//  Created by Edison Gudiño on 12/10/20.
+//  Copyright © 2020 Edison Gudiño. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
